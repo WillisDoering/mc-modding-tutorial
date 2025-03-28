42190ae18910022c0ceb5aa33a7a1cde73bb80f9
William Doering <willis@willisdoering.net>
2025-03-27 20:41:39 -0500

    Fixed MOD_ID

--- src/main/java/net/willisdoering/tutorialmod/TutorialMod.java
+++ src/main/java/net/willisdoering/tutorialmod/TutorialMod.java
@@ -18,7 +18,7 @@ import org.slf4j.Logger;
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "tutorial-mod";
+    public static final String MOD_ID = "tutorial_mod";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
