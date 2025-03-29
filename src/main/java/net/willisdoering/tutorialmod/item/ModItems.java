dcb5bcb34b59734ec11afca5717d05a20512a5ab
William Doering <willis@willisdoering.net>
2025-03-29 11:14:57 -0500

    Added chisel

--- src/main/java/net/willisdoering/tutorialmod/item/ModItems.java
+++ src/main/java/net/willisdoering/tutorialmod/item/ModItems.java
@@ -7,6 +7,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.willisdoering.tutorialmod.TutorialMod;
+import net.willisdoering.tutorialmod.item.custom.ChiselItem;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -16,6 +17,8 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel",
+            () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
