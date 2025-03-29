bbd7dddf901089f7313421e31d9a8834f028eaa2
William Doering <willis@willisdoering.net>
2025-03-28 21:08:06 -0500

    Added block of alexandrite

--- src/main/java/net/willisdoering/tutorialmod/TutorialMod.java
+++ src/main/java/net/willisdoering/tutorialmod/TutorialMod.java
@@ -13,6 +13,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.willisdoering.tutorialmod.block.ModBlocks;
 import net.willisdoering.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -34,6 +35,7 @@ public class TutorialMod {
         MinecraftForge.EVENT_BUS.register(this);
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
 
         // Register the item to a creative tab
@@ -51,6 +53,10 @@ public class TutorialMod {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.ALEXANDRITE);
             event.accept(ModItems.RAW_ALEXANDRITE);
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.ALEXANDRITE_BLOCK);
         }
     }
 
