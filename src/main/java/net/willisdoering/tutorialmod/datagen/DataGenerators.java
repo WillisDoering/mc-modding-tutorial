2e4fe2139c5f318ad535219ba467df3767b68f91
William Doering <willis@willisdoering.net>
2025-03-31 21:25:16 -0500

    Most recipes working for generator. Smelting still hand-added

--- src/main/java/net/willisdoering/tutorialmod/datagen/DataGenerators.java
+++ src/main/java/net/willisdoering/tutorialmod/datagen/DataGenerators.java
@@ -32,6 +32,7 @@ public class DataGenerators {
                 packOutput,
                 Collections.emptySet(),
                 List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTableProvider::new, LootContextParamSets.BLOCK)), lookupProvider));
+        generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput, lookupProvider));
 
         BlockTagsProvider blockTagsProvider = new ModBlockTagProvider(packOutput, lookupProvider, existingFileHelper);
         generator.addProvider(event.includeServer(), blockTagsProvider);
